14fda77e178806bb8cad08401db7ed40b76bf48d
Udo Seidel <udoseidel@gmx.de>
2022-10-22 01:52:19 +0200

    working on github actions part #2

--- getsources.sh
+++ getsources.sh
@@ -16,6 +16,12 @@ MYSHORTCOMMIT=`echo $MYCOMMIT| cut -c1-7`
 # copy the also needed patch file(s) to the RPMBUILD space
 # again this is different from the $GITHUB_WORKSPACE
 ###cp *.patch /github/home/rpmbuild/SOURCES
+#
+## copy the also needed patch file(s) to the RPMBUILD space
+# again this is different from the $GITHUB_WORKSPACE
+cp * /github/home/rpmbuild/SOURCES
+rm /github/home/rpmbuild/SOURCES/*spec
+
 
 # Now download the sources to the correspoding RPMBUILD directory
 cd /github/home/rpmbuild/SOURCES
