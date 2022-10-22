b6c26e40758c03824f8865160cef7bb24dfcbcda
Udo Seidel <udoseidel@gmx.de>
2022-10-22 10:11:33 +0200

    OBS build added

---
+++ submitobsbuild.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash -x
+
+PATH=/usr/bin:/usr/sbin/
+export PATH
+
+MYDATE=`date`
+
+# checkout the OSB package
+osc co home:useidel fetchmail7
+
+cd home*
+
+# copy (updated) files to working directory
+# note that the content from github is now in the upper directory
+cp ../*spec fetchmail7/
+cp ../*.tar.gz fetchmail7/
+
+# mark files for update if there are new ones
+#cd dummy
+#osc add *spec
+#cd ..
+
+# upload changed content
+osc ci -m "Update from $MYDATE" fetchmail7
+
+
+
